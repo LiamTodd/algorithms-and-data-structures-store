edd661b3b4bf5690d52d99ef96c2e35968307dcd
LiamTodd <toddliam26@gmail.com>
2022-06-09 13:44:34 +1000

    basic sorts

---
+++ insertion_sort.py
@@ -0,0 +1,17 @@
+def insertion_sort(L):
+
+    for i in range(1, len(L)):
+        current = L[i]
+        j = i - 1
+        while j >= 0 and current < L[j]:
+            # shift j'th element up and update j
+            L[j+1] = L[j]
+            j -= 1
+
+        # reinsert
+        L[j+1] = current
+
+    return L
+
+print(insertion_sort([1,2,3,4,5]))
+print(insertion_sort([5,4,3,2,1]))
---
+++ selection_sort.py
@@ -0,0 +1,21 @@
+def selection_sort(L):
+    for i in range(len(L)):
+        min_index = get_min_index(L, i)
+        if i != min_index:
+            swap(L, i, min_index)
+    return L
+
+def get_min_index(L, start_index):
+    min_index = start_index
+    for i in range(start_index + 1, len(L)):
+        if L[i] < L[min_index]:
+            min_index = i
+    return min_index
+
+def swap(L, i, j):
+    temp = L[i]
+    L[i] = L[j]
+    L[j] = temp
+
+print(selection_sort([1,2,3,4,5]))
+print(selection_sort([5,4,3,2,1]))
