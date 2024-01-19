564a1a717138e6f2ea7402dc34171481e61e84f8
LiamTodd <toddliam26@gmail.com>
2024-01-19 21:45:00 +1100

    Implement linear probe delete.

--- 2024/data_structures/hash_table.py
+++ 2024/data_structures/hash_table.py
@@ -2,20 +2,21 @@ class LinearProbeHashTable:
     def __init__(self, size):
         self.size = size
         self.table = [None for _ in range(size)]
+        self.items = 0
 
     def insert(self, key, value):
+        if self.items == self.size:
+            print("Table full")
+            return
+
         index = self._hash(key)
-        first_check = index
         # traverse the bucket in circular fashion
         while self.table[index] is not None:
             index = (index + 1) % self.size
-            # ensure table is not full
-            if index == first_check:
-                print("Table full")
-                return None
 
         # empty spot found, insert here
         self.table[index] = (key, value)
+        self.items += 1
 
     def search(self, key):
         index = self._hash(key)
@@ -33,11 +34,32 @@ class LinearProbeHashTable:
         return False
 
     def delete(self, key):
-        # linear probe from index
-        # empty cell when found
-        # look forward from bucket, checking hash values of keys
-        # look for the end of the bucket, move that value into the empty spot
-        pass
+        index = self._hash(key)
+        first_check = index
+        item_deleted = False
+        # traverse the bucket in circular fashion
+        while self._table[index] is not None:
+            if self._table[index][0] == key:
+                self._table[index] = None
+                item_deleted = True
+                self.items -= 1
+                break
+            else:
+                index = (index + 1) % self.size
+                # ensure not double-checking
+                if index == first_check:
+                    print("Item not found")
+                    return None
+        if not item_deleted:
+            print("Item not found")
+            return
+        # rehash elements in the primary bucket
+        rehash_index = index + 1
+        while self._table[rehash_index] is not None:
+            entry = self._table[rehash_index]
+            self._table[rehash_index] = None
+            self.insert(entry[0], entry[1])
+            rehash_index = (rehash_index + 1) % self.size
 
     def _hash(self, key):
         return int(key) % self.size
